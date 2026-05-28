cde4e89b4904edb85d5e0d2b3b919b8b39666879
andyrueda439 <ajrueda@uamv.edu.ni>
2026-05-28 11:48:44 -0500

    Agregando version corregida de las tablas

--- Ses1JuevesS11.sql
+++ Ses1JuevesS11.sql
@@ -11,3 +11,27 @@ update Academico.Carrera set precio = 2000.99, updated_at = getdate() where id =
 insert into Seguridad.Usuario(cif, nombres, apellidos, pw) values('401', 'Pedro', 'Pascal', HASHBYTES('SHA2_256', 'Temp2026*'));
 
 select * from Seguridad.Usuario
+
+insert into Seguridad.Cargo(nombre)
+values('Administrador');
+
+insert into Seguridad.Usuario
+(
+    cif,
+    nombres,
+    apellidos,
+    fechaNac,
+    pw,
+    email,
+    idCargo
+)
+values
+(
+    '401',
+    'Pedro',
+    'Pascal',
+    '1990-04-15',
+    HASHBYTES('SHA2_256', 'Temp2026*'),
+    'pedro@universidad.com',
+    1
+);
